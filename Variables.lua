fb988db7c9e2b448d250fcb2b046b915b7608c76
Evgenii Starostin <pareylook@gmail.com>
2022-10-10 21:10:48 +0200

    Create Variables.lua

---
+++ Variables.lua
@@ -0,0 +1,23 @@
+-- VARIABLES --
+
+--Variables types:
+--nil
+--number
+--string
+--function
+--CFunction
+--usedata
+--table
+
+
+number = 100
+print(number)
+print("type of int is " ..type(number)) -- Exaple how to join strings
+
+float_number = 1.5
+print(float_number)
+print("type of float is " .. type(float_number))
+
+string = "example text"
+print(string)
+print("type of sttring is " ..type(string))
