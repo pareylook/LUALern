be8de780ee203c467cd9bf086305265945d1427c
Evgenii Starostin <pareylook@gmail.com>
2022-10-13 08:18:20 +0200

    Create Conditionals.lua

---
+++ Conditionals.lua
@@ -0,0 +1,17 @@
+-- BASE CONDITIONALS --
+
+-- value1 == value2 -- equal 
+-- value1 ~= value2 -- not equal
+
+option = 3
+text = ""
+
+if option == 1 then
+    text = "one"
+elseif option == 2 then
+    text = "two"
+else text = 3
+    text = "three"
+end
+
+print(text)
