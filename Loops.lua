82bd6b33f8e5af28cb63f2d699959446c2d451c8
Evgenii Starostin <pareylook@gmail.com>
2022-10-17 14:59:05 +0200

    Create Loops.lua

---
+++ Loops.lua
@@ -0,0 +1,28 @@
+math.randomseed(os.time()) -- use different seed every time
+-- For Loop --
+print("-- For Loop --")
+for count = 1, 10 do
+    x_pos = math.random(0, 100)
+    y_pos = math.random(0, 100)
+    print(count.." count is: ".."x = "..x_pos.." y = "..y_pos)
+end
+print("------------")
+print("            ")
+print("-- For Loop with custom step --")
+-- For Loop with custom step--
+for count = 1, 10, 5 do
+    x_pos = math.random(0, 100)
+    y_pos = math.random(0, 100)
+    print(count.." count is: ".."x = "..x_pos.." y = "..y_pos)
+end
+print("------------")
+print("            ")
+print("-- While Loop --`")
+-- While Loop --
+number = 0
+while number < 10 do
+    x_pos = math.random(0, 100)
+    y_pos = math.random(0, 100)
+    print(number.." number is: ".."x = "..x_pos.." y = "..y_pos)
+    number = number + 1
+end
